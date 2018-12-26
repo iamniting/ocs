111605a0c5f9072753ed67dbdf036dc6fc0f684c
nigoyal <nigoyal@redhat.com>
2018-12-26 17:15:06 +0530

    changing subdir script

--- scripts/sub-directory.sh
+++ scripts/sub-directory.sh
@@ -1,17 +1,21 @@
 # it creates a directories and sub-directories & verify checksum
 # sh script.sh
 
-mkdir -p 01/{01..10}/{01..10}/{01..10}
 
-echo "hello we are writing data into files" | tee 01/{01..10}/{01..10}/{01..10}/file
-echo "hello we are writing data into files" | tee 01/{01..10}/{01..10}/file
-echo "hello we are writing data into files" | tee 01/{01..10}/file
-echo "hello we are writing data into files" | tee 01/file
+parentDir=tmp
 
-hex=`find 01 -type f -name file -exec md5sum {} + | awk '{print $1}' | sort | md5sum`
+mkdir -p $parentDir/{01..10}/{01..10}/{01..10}
+
+echo "hello we are writing data into files" | tee $parentDir/{01..10}/{01..10}/{01..10}/file
+echo "hello we are writing data into files" | tee $parentDir/{01..10}/{01..10}/file
+echo "hello we are writing data into files" | tee $parentDir/{01..10}/file
+echo "hello we are writing data into files" | tee $parentDir/file
+
+hex=`find $parentDir -type f -name file -exec md5sum {} + | awk '{print $1}' | sort | md5sum`
 
 if [ "$hex" == "d256be3c7826e725825c4b7c6f8c40a7  -" ]; then
     echo "checksum is correct"
+    rm -rf $parentDir
 else
     echo "checksum is not correct"
 fi
