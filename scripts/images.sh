1b2657af02740e16233539d7c7ba8ab31573de8e
nigoyal <nigoyal@redhat.com>
2019-02-18 11:45:01 +0530

    changing version of images from 3.10 to 3.11

--- scripts/images.sh
+++ scripts/images.sh
@@ -4,30 +4,30 @@
 ### eg. sh images.sh
 
 
-docker pull rhel7/etcd:3.2.22
+docker pull rhel7/etcd:3.2.22 &
 
-docker pull openshift3/ose-node:v3.10
-docker pull openshift3/ose-haproxy-router:v3.10
-docker pull openshift3/ose-deployer:v3.10
-docker pull openshift3/ose-control-plane:v3.10
-docker pull openshift3/ose-web-console:v3.10
-docker pull openshift3/ose-docker-registry:v3.10
-docker pull openshift3/ose-pod:v3.10
+docker pull openshift3/ose-node:v3.11 &
+docker pull openshift3/ose-haproxy-router:v3.11 &
+docker pull openshift3/ose-deployer:v3.11 &
+docker pull openshift3/ose-control-plane:v3.11 &
+docker pull openshift3/ose-web-console:v3.11 &
+docker pull openshift3/ose-docker-registry:v3.11 &
+docker pull openshift3/ose-pod:v3.11 &
 
-docker pull openshift3/oauth-proxy:v3.10
-docker pull openshift3/registry-console:v3.10
+docker pull openshift3/oauth-proxy:v3.11 &
+docker pull openshift3/registry-console:v3.11 &
 
-docker pull openshift3/logging-fluentd:v3.10
-docker pull openshift3/logging-curator:v3.10
-docker pull openshift3/logging-auth-proxy:v3.10
-docker pull openshift3/logging-kibana:v3.10
-docker pull openshift3/logging-elasticsearch:v3.10
+docker pull openshift3/logging-fluentd:v3.11 &
+docker pull openshift3/logging-curator:v3.11 &
+docker pull openshift3/logging-auth-proxy:v3.11 &
+docker pull openshift3/logging-kibana:v3.11 &
+docker pull openshift3/logging-elasticsearch:v3.11 &
 
-docker pull openshift3/metrics-heapster:v3.10
-docker pull openshift3/metrics-hawkular-metrics:v3.10
-docker pull openshift3/metrics-cassandra:v3.10
-docker pull openshift3/metrics-schema-installer:v3.10
+docker pull openshift3/metrics-heapster:v3.11 &
+docker pull openshift3/metrics-hawkular-metrics:v3.11 &
+docker pull openshift3/metrics-cassandra:v3.11 &
+docker pull openshift3/metrics-schema-installer:v3.11 &
 
-docker pull rhgs3/rhgs-server-rhel7:v3.10
-docker pull rhgs3/rhgs-volmanager-rhel7:v3.10
-docker pull rhgs3/rhgs-gluster-block-prov-rhel7:v3.10
+docker pull rhgs3/rhgs-server-rhel7:v3.11 &
+docker pull rhgs3/rhgs-volmanager-rhel7:v3.11 &
+docker pull rhgs3/rhgs-gluster-block-prov-rhel7:v3.11 &
