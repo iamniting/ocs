cc127b105a0cba0e5fe70f967074affc81fcadbf
Nitin Goyal <nigoyal@redhat.com>
2019-06-11 11:59:08 +0530

    Add script cirros-create-and-wait.sh
    This script will create cirros pod and wait for it to be in Running
    state. Once the first pod is Up and Running it will go to next one.

---
+++ scripts/cirros-create-and-wait.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+### it creates n cirros app pods
+### it depends on cirros-create.sh
+### eg. sh cirros-create-and-wait.sh
+
+
+for i in {001..020}; do
+    sh cirros-create-with-io.sh $i claim$i;
+
+    echo >> cirros.log
+    echo `date` - "cirros"$i " creation request sent" >> cirros.log
+
+    while true; do
+        status=`oc get pod --no-headers -l deploymentconfig=cirros$i \
+            -o=custom-columns=:.status.containerStatuses[0].ready,:.status.phase`
+        if [ "$status" == "true      Running" ]; then
+            echo `date` - "cirros"$i " is Running successfully" >> cirros.log
+            break
+        fi
+    done
+done
