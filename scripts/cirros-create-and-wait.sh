d951b1e955b201cfeee33011338739873fd79477
Nitin Goyal <nigoyal@redhat.com>
2019-06-13 15:11:03 +0530

    Change appropriate name of script

--- scripts/cirros-create-and-wait.sh
+++ scripts/cirros-create-and-wait.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 ### it creates n cirros app pods
-### it depends on cirros-create.sh
+### it depends on cirros-create-with-io.sh
 ### eg. sh cirros-create-and-wait.sh
 
 
 for i in {001..020}; do
     sh cirros-create-with-io.sh $i claim$i;
 
-    echo >> cirros.log
     echo `date` - "cirros"$i " creation request sent" >> cirros.log
 
     while true; do
@@ -16,6 +15,7 @@ for i in {001..020}; do
             -o=custom-columns=:.status.containerStatuses[0].ready,:.status.phase`
         if [ "$status" == "true      Running" ]; then
             echo `date` - "cirros"$i " is Running successfully" >> cirros.log
+            echo | tee -a cirros.log
             break
         fi
     done
--- scripts/pvc-create-and-wait.sh
+++ scripts/pvc-create-and-wait.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 ### it creates n pvc's with logs which contains time of requests
-### eg. sh pvc-create-bulk-log.sh
+### eg. sh pvc-create-and-wait.sh
 
 
 for i in {001..020}; do
     sh pvc-create.sh claim$i 1;
 
-    echo | tee -a pvctrace.log
-    echo `date` - "pvc claim" $i " creation request sent" | tee -a pvctrace.log
+    echo `date` - "PVC claim"$i "creation request sent" | tee -a pvctrace.log
 
     while true; do
         var="$(oc get pvc claim$i -o=custom-columns=:.status.phase --no-headers)"
 
         if [ "${var}" == 'Bound' ]; then
-            echo `date` - "pvc claim" $i " creation successful" | tee -a pvctrace.log
+            echo `date` - "PVC claim"$i "creation successful" | tee -a pvctrace.log
+            echo | tee -a pvctrace.log
             break
         fi
     done
